2395b11bfe4ab311f24a8384e2a1896430458ca3
luocong2016 <luocong2016@126.com>
2020-12-03 10:22:50 +0800

    Update Setting.vue
    看起来不像 vue3

--- my-site/src/views/Setting.vue
+++ my-site/src/views/Setting.vue
@@ -1,19 +1,18 @@
-<template>
-  <div>
-    <h1>This is a setting page</h1>
-    <p>store count is: {{ count }}</p>
-  </div>
-</template>
-
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Setting",
-  computed: {
-    count() {
-      return this.$store.state.count;
-    },
-  },
+  
+  setup() {
+    const store = useStore();
+    
+    return () => (
+      <div>
+        <h1>This is a setting page</h1>
+        <p>store count is: { store.state.count }</p>
+      </div>
+    );
+  }
 });
 </script>
